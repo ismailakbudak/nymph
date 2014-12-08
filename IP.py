bcf7fc45cebf76f851802c5fc0bed4a5b3355b65
Ismail Akbudak <ismail.akbudak@lab2023.com>
2014-12-08 20:32:19 +0200

    create init file

--- IP.py
+++ IP.py
@@ -1,8 +1,8 @@
 from Nymph import *
-ismail=nymphdata("ismail","127.0.0.1",8592)
-erdem=nymphdata('erdem','127.0.0.1',7711)
-ferhat=nymphdata('ferhat','127.0.0.1',8812)
-alptug=nymphdata('alptug','127.0.0.1',8813)
+ismail=nymphdata("ismail","127.0.0.1",8658)
+erdem=nymphdata('erdem','127.0.0.1',8778)
+ferhat=nymphdata('ferhat','127.0.0.1',8669)
+alptug=nymphdata('alptug','127.0.0.1',9555)
 ali=nymphdata('ali','127.0.0.1',8890)
 veli=nymphdata('veli','127.0.0.1',8891)
 firuze=nymphdata('firuze','127.0.0.1',8892)
